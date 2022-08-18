abff8eef7fd7cc0d9b5e68076dd407c20e3d4dd3
Nathan <nathan@pop-os.localdomain>
2022-08-18 20:52:50 +0100

    commented and split heavy functions

--- sentiment.py
+++ sentiment.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c4901f5b91b50009dd9e7334f609d61eeb2a79cc5938a263286c6a521e488b8a
-size 7734
+oid sha256:15d92fa892da15d4dca5529670c61330f6698f636632a1c8d01a1ddab139a416
+size 8217
